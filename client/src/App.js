9ff10709e1d4c6ef3fd9b98d9d3be6eec878241e
Wiam Nasr <wiamnasr@gmail.com>
2022-03-08 17:18:30 +0000

    more lean app.js

--- client/src/App.js
+++ client/src/App.js
@@ -4,28 +4,15 @@ import Landing from "./pages/Landing";
 
 // Importing components
 import Navbar from "./Components/Navbar/Navbar";
-import NavItem from "./Components/Navbar/NavItem";
-import DropdownMenu from "./Components/Navbar/DropdownMenu";
-
-import developmentSvg from "./Assets/svg/devPlanner.svg";
-
-import navOptions from "./Assets/svg/selectDropdown.svg";
 
 const App = () => (
-
 	<>
-		<Navbar>
-			<NavItem href="/about/this/site" icon={developmentSvg} />
-			<NavItem icon={navOptions}>
-				<DropdownMenu />
-			</NavItem>
-		</Navbar>
+		<Navbar />
 		<Routes>
-      <Route path="/" element={<Landing />} />
-      <Route path="/myPlan" element={<MyPlan />} />
-	  </Routes>
+			<Route path="/" element={<Landing />} />
+			<Route path="/myPlan" element={<MyPlan />} />
+		</Routes>
 	</>
-
 );
 
 export default App;
--- client/src/Components/Navbar/Navbar.js
+++ client/src/Components/Navbar/Navbar.js
@@ -1,12 +1,25 @@
 import classes from "./Navbar.module.css";
 
+// import NavItem from "./NavItem";
+
+import NavItem from "../Navbar/NavItem";
+import DropdownMenu from "../Navbar/DropdownMenu";
+
+import developmentSvg from "../../Assets/svg/development.svg";
+
+import navOptions from "../../Assets/svg/selectDropdown.svg";
 
 
-const Navbar = (props) => {
+
+const Navbar = () => {
     return (
 			<nav className={classes.navbar}>
 				<ul className={classes.navbarNav}>
-                    {props.children}
+					{/* {props.children} */}
+					<NavItem href="/about/this/site" icon={developmentSvg} />
+					<NavItem icon={navOptions}>
+						<DropdownMenu />
+					</NavItem>
 				</ul>
 			</nav>
 		);
