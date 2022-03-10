5e6fbde7519dfd4a60a0fad44a1eb47adb9cada9
Wiam Nasr <wiamnasr@gmail.com>
2022-03-10 16:17:57 +0000

    added tasks fetcher and populator

--- client/src/pages/MyPlan.js
+++ client/src/pages/MyPlan.js
@@ -1,15 +1,13 @@
-import Card from '../Components/UI/Card/Card';
+import Card from "../Components/UI/Card/Card";
+
+import Tasks from "../Components/Tasks/Tasks";
 
 const MyPlan = () => {
 return (
 	<main role="main">
 		<Card>
-			<h1>Home</h1>
-			<p>
-				Starter kit for full-stack JavaScript projects. For more information,
-				see the wiki:
-			</p>
-			<a href="https://github.com/textbook/starter-kit/wiki">Wiki</a>
+			<h1>Tasks</h1>
+			<Tasks />
 		</Card>
 	</main>
 );
