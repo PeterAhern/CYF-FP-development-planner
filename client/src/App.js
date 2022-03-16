b442c6fef75f34ef6f1c392bb5f9ad004ab7cf30
Wiam Nasr <wiamnasr@gmail.com>
2022-03-16 13:33:27 +0000

    fixing google authentication

--- client/src/App.js
+++ client/src/App.js
@@ -3,20 +3,13 @@ import MyPlan from "./pages/MyPlan";
 import Landing from "./pages/Landing";
 import { useState } from "react";
 
-const App = () => {
-	// pass the value of the user email from Landing to My Plan
-	const userEmail = (email) => email;
 
-
-// Importing components
-import Navbar from "./Components/Header/Navbar/Navbar";
 
 const App = () => {
 	const [user, setUser] = useState(" ");
 	const userChange=(user) =>(setUser(user));
 	return (
 		<>
-			<Navbar />
 			<Routes>
 				<Route path="/" element={<Landing user={userChange} />} />
 				<Route path="/myPlan" element={<MyPlan user = { user } />} />
