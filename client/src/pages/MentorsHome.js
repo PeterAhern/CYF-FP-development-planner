1c2c2c157ac69657e8e7064e0c64826bd46f7900
Halla Sulaiman <86550752+Halla-S@users.noreply.github.com>
2022-03-18 19:52:02 +0000

    update mentors

--- client/src/pages/MentorsHome.js
+++ client/src/pages/MentorsHome.js
@@ -1,11 +1,12 @@
 import Navbar from "../Components/Header/Navbar/Navbar";
 import Tasks from "../Components/Tasks/Tasks";
 import Search from "../Components/Search";
+import AllUsers from "../Components/AllUsers";
 const MentorsHome = () => {
 	return (
 		<>
 			<Navbar />
-			<Search  />
+			<AllUsers />
 			<h2> welcome mentor</h2>
 			<Tasks />
 		</>
