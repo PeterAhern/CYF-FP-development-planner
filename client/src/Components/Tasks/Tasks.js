14b6c2f2f932ff978182203e8e153a06cbffc574
Peter Ahern <peterahern87@gmail.com>
2022-03-22 14:48:27 +0000

    Task Card Info - DueDate & Status added
    Can now view due date and status for each task in the task card list.

--- client/src/Components/Tasks/Tasks.js
+++ client/src/Components/Tasks/Tasks.js
@@ -2,12 +2,17 @@ import React, { useState, useEffect, useCallback } from "react";
 import Card from "../UI/Card/Card";
 import PopUpForm from "./PopUpForm";
 import TaskForm from "../TaskForm/TaskForm";
+//for date formatting on task card
+	import moment from "moment";
 
 const Tasks = ({ refresh, refreshFunc }) => {
 	const [tasks, setTasks] = useState([]);
 	const [isLoading, setIsLoading] = useState(false);
 	const [error, setError] = useState(null);
-	const [editingTask, setEditingTask] = useState( { editing: false, initialFormState: {} });
+	const [editingTask, setEditingTask] = useState({
+		editing: false,
+		initialFormState: {},
+	});
 	//states for pop up edit task form
 	const [isOpen, setIsOpen] = useState(false);
 	const togglePopup = () => {
@@ -29,7 +34,7 @@ const Tasks = ({ refresh, refreshFunc }) => {
 				statusId: tasks[index].status_id,
 			},
 		});
-			togglePopup();
+		togglePopup();
 	};
 
 	const fetchTasksHandler = useCallback(async () => {
@@ -42,7 +47,6 @@ const Tasks = ({ refresh, refreshFunc }) => {
 				throw new Error("Something went wrong!");
 			}
 			const data = await response.json();
-      
 			const loadedTasks = [];
 			for (const key in data) {
 				loadedTasks.push({
@@ -56,7 +60,6 @@ const Tasks = ({ refresh, refreshFunc }) => {
 				});
 			}
 			setTasks(loadedTasks);
-
 		} catch (error) {
 			setError(error.message);
 		}
@@ -66,7 +69,6 @@ const Tasks = ({ refresh, refreshFunc }) => {
 	useEffect(() => {
 		fetchTasksHandler();
 	}, [fetchTasksHandler, refresh]);
-
 
 	const deleteTask = async (id) => {
 		const response = await fetch(`/api/tasks/${id}`, {
@@ -78,72 +80,83 @@ const Tasks = ({ refresh, refreshFunc }) => {
 		}
 	};
 
+	const statusShower = (id) => {
+			if (id === 1) {
+				return "Not Started";
+			} else if (id === 2) {
+				return "In Progress";
+			} else if (id === 3) {
+				return "Complete";
+			} else if (id === 4) {
+				return "N/A";
+			}
+		};
+
+
 	let content = <p>Found no tasks.</p>;
 
-    if (tasks.length > 0) {
+	if (tasks.length > 0) {
+		content = tasks.map((task, index) => (
+			<Card key={task.id}>
+				<h1 className="card_title">{task.title}</h1>
+				<h3 className="card_due_date">
+					Due: {moment.utc(task.due_date).format("DD/MM/YY")}
+				</h3>
+				<h3 className="card_status">{statusShower(task.status_id)}</h3>
+				<button
+					className="btn btn-danger"
+					onClick={() => {
+						window.confirm("Are you sure you want to delete?") &&
+							deleteTask(task.id);
+					}}
+				>
+					Delete
+				</button>
+				<div>
+					<input
+						type="button"
+						className="btn btn-danger"
+						value="Edit Task"
+						onClick={(e) => editTaskHandler(e, index)}
+					/>
+					{isOpen && (
+						<PopUpForm
+							content={
+								<>
+									<b>Edit your task</b>
+									<TaskForm
+										refreshFunc={refreshFunc}
+										editingTask={editingTask}
+										setEditingTask={setEditingTask}
+									/>
+								</>
+							}
+							handleClose={togglePopup}
+						/>
+					)}
+				</div>
+				<div>
+					{task.evidence && (
+						<h4>
+							<a href={task.evidence} target="_blank" rel="noreferrer">
+								Evidence
+							</a>
+						</h4>
+					)}
+				</div>
+			</Card>
+		));
+	}
 
-        content = tasks.map((task, index) => (
-					<Card key={task.id}>
-						<h1>{task.title}</h1>
-						<button
-							className="btn btn-danger"
-							onClick={() => {
-								window.confirm("Are you sure you want to delete?") &&
-									deleteTask(task.id);
-							}}
-						>
-							Delete
-						</button>
-						<div>
-							<input
-								type="button"
-								className="btn btn-danger"
-								value="Edit Task"
-								onClick={(e) => editTaskHandler(e, index)}
-							/>
-							{isOpen && (
-								<PopUpForm
-									content={
-										<>
-											<b>Edit your task</b>
-											<TaskForm
-												refreshFunc={refreshFunc}
-												editingTask={editingTask}
-												setEditingTask={setEditingTask}
-											/>
-										</>
-									}
-									handleClose={togglePopup}
-								/>
-							)}
-						</div>
-						<div>
-							{task.evidence && (
-								<h4>
-									<a href={task.evidence} target="_blank" rel="noreferrer">
-										Evidence
-									</a>
-								</h4>
-							)}
-						</div>
-					</Card>
-				));
-    }
+	if (error) {
+		content = <p>{error}</p>;
+	}
 
-    if (error) {
-        content = <p>{error}</p>;
-    }
+	if (isLoading) {
+		content = <p>Loading...</p>;
+	}
 
-    if (isLoading) {
-        content = <p>Loading...</p>;
-    }
-
-	return (
-		<>
-			{content}
-		</>
-	);
-
-};
+	return <>{content}</>;
+};;
 
 export default Tasks;
