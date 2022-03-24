a9dc6f41488b3921f41216ca408b99e87bb8aa5b
Wiam <wiamnasr@gmail.com>
2022-03-24 11:40:24 +0000

    added hamburger menu

--- client/src/Components/Header/Navbar/Navbar.js
+++ client/src/Components/Header/Navbar/Navbar.js
@@ -1,5 +1,3 @@
-import classes from "./Navbar.module.css";
-
 import NavItem from "../Navbar/NavItem";
 import DropdownMenu from "../Navbar/DropdownMenu";
 
@@ -8,10 +6,50 @@ import developmentSvg from "../../../Assets/svg/development.svg";
 import navOptions from "../../../Assets/svg/selectDropdown.svg";
 import Goal from "./Goal";
 
+// import SideDrawerNavigation from "./SideDrawerNavigation/SideDrawerNavigation";
+
+import React, { useState } from "react";
+import {
+	Nav,
+	NavLink,
+	Bars,
+	NavMenu,
+} from "./Navbar.style";
+
 const Navbar = ({ logoutHandler }) => {
+	const [toggleHamburgerMenu, setToggleHamburgerMenu] = useState(false);
+
+	const toggleHamburgerMenuHandler = () => setToggleHamburgerMenu(!toggleHamburgerMenu);
 	return (
-		<nav className={classes.navbar}>
-			<ul className={classes.navbarNav}>
+		<>
+			<Nav>
+				<Bars onClick={toggleHamburgerMenuHandler} />
+				{/* {toggleHamburgerMenu && <SideDrawerNavigation />} */}
+
+				<Goal />
+				<ul className="navbarNav">
+					<NavMenu>
+						<NavItem
+							navText="Dev Planner"
+							href="/plan"
+							icon={developmentSvg}
+						/>
+						<NavItem navText="Options" icon={navOptions}>
+							<DropdownMenu logoutHandler={logoutHandler} />
+						</NavItem>
+					</NavMenu>
+				</ul>
+			</Nav>
+		</>
+	);
+
+};
+
+export default Navbar;
+
+
+/*
+	<ul className={"navbarNav"}>
 				<Goal />
 				<NavItem navText="Dev Planner" href="/plan" icon={developmentSvg} />
 				<NavItem navText="Options" icon={navOptions}>
@@ -20,9 +58,5 @@ const Navbar = ({ logoutHandler }) => {
 					/>
 				</NavItem>
 			</ul>
-		</nav>
-	);
 
-};
-
-export default Navbar;
+*/
