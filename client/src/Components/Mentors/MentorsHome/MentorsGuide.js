3ba29202a70c3f6732ea9840606ae532942023f6
Halla Sulaiman <86550752+Halla-S@users.noreply.github.com>
2022-03-31 18:03:40 +0100

    Adding the Mentors guide page

---
+++ client/src/Components/Mentors/MentorsHome/MentorsGuide.js
@@ -0,0 +1,32 @@
+import "./MentorsGuide.css";
+const MentorsGuide=()=>{
+    return (
+			<div className="guide">
+				<h2>Mentor Guide</h2>
+				<p>
+					The essential need-to-know list for Mentors @CYF About the role:
+					Involves supporting two trainees on their career journey, helping with
+					goal setting, identifying the personal and technical skills needed for
+					junior development roles. Activities: Have regular 1:1 sessions with
+					your mentee Help facilitate mock interviews Review CVs and give
+					feedback Goal setting and career guidance Work with graduates on their
+					Elements. Starting the Relationship During your first meeting with
+					your graduate - mentee you may wish to do the following: Explain the
+					purpose of the mentoring relationship Decide together the format of
+					the meetings and how these will work Explain your role as mentor and
+					what you will commit to Clarify what is expected behaviour from
+					mentees and their role The Mentoring Sessions During the sessions you
+					may wish to do the following: Review experience Identify goal and
+					objectives Provide feedback Identify strengths and achievements
+					Identify areas of development Explore options Agree support needs Set
+					targets for future actions Create networking opportunities for mentees
+					to gain experience Ending the Relationship Mentoring should end six
+					months after Demo Day or when a mentee secures employment. Summarise
+					and celebrate achievements Help your mentee to identify their next
+					steps Both mentee and mentor should reflect on the effectiveness of
+					the relationship
+				</p>
+			</div>
+		);
+};
+export default MentorsGuide;
