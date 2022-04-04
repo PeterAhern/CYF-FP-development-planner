cbce330df2ec40663ab15bcc86a0db0bbfcc9b29
Halla Sulaiman <86550752+Halla-S@users.noreply.github.com>
2022-04-04 07:43:45 +0100

    mentor guide

--- client/src/pages/MentorsGuide.js
+++ client/src/pages/MentorsGuide.js
@@ -1,38 +1,93 @@
 import { MentorsGuideStyles } from "./MentorsGuide.styles";
-import Navbar from "../Components/Header/Navbar/Navbar"
+import Navbar from "../Components/Header/Navbar/Navbar";
 
 const MentorsGuide=()=>{
     return (
-			<div>
+			<main>
 				<Navbar />
 				<MentorsGuideStyles>
-					<h2>Mentor Guide</h2>
-					<p>
-						The essential need-to-know list for Mentors @CYF About the role:
-						Involves supporting two trainees on their career journey, helping
-						with goal setting, identifying the personal and technical skills
-						needed for junior development roles. Activities: Have regular 1:1
-						sessions with your mentee Help facilitate mock interviews Review CVs
-						and give feedback Goal setting and career guidance Work with
-						graduates on their Elements. Starting the Relationship During your
-						first meeting with your graduate - mentee you may wish to do the
-						following: Explain the purpose of the mentoring relationship Decide
-						together the format of the meetings and how these will work Explain
-						your role as mentor and what you will commit to Clarify what is
-						expected behaviour from mentees and their role The Mentoring
-						Sessions During the sessions you may wish to do the following:
-						Review experience Identify goal and objectives Provide feedback
-						Identify strengths and achievements Identify areas of development
-						Explore options Agree support needs Set targets for future actions
-						Create networking opportunities for mentees to gain experience
-						Ending the Relationship Mentoring should end six months after Demo
-						Day or when a mentee secures employment. Summarise and celebrate
-						achievements Help your mentee to identify their next steps Both
-						mentee and mentor should reflect on the effectiveness of the
-						relationship
-					</p>
+					<h2>
+						Mentor Guide: The essential need-to-know list for Mentors @CYF.
+					</h2>
+					<section>
+						<h3>About the role:</h3>
+						<p>
+							Involves supporting two trainees on their career journey, helping
+							with goal setting,<br></br> identifying the personal and technical
+							skills needed for junior development roles.
+						</p>
+					</section>
+					<section>
+						<h3>Activities:</h3>
+						<ul>
+							<li>Have regular 1:1 sessions with your mentee</li>
+							<li>
+								Help facilitate mock interviews Review CVs and give feedback
+							</li>
+							<li>Goal setting and career guidance </li>
+							<li>Work with graduates on their Elements.</li>
+						</ul>
+					</section>
+					<section>
+						<h3>Starting the Relationship</h3>
+						<p>
+							During your first meeting with your graduate - mentee you may wish
+							to do the following:
+						</p>
+					</section>
+					<section>
+						<ul>
+							<li>Explain the purpose of the mentoring relationship</li>
+							<li>
+								Decide together the format of the meetings and how these will
+							</li>
+							<li>Explain your role as mentor and what you will commit to</li>
+							<li>
+								Clarify what is expected behaviour from mentees and their role
+							</li>
+						</ul>
+					</section>
+					<section>
+						<h3>The Mentoring Sessions</h3>
+						<p>During the sessions you may wish to do the following: </p>
+						<ul>
+							<li>Review experience.</li>
+							<li>Identify goal and objectives.</li>
+							<li>Provide feedback. </li>
+							<li>Identify areas of development.</li>
+							<li>Identify strengths and achievements.</li>
+							<li>Explore options.</li>
+							<li>Agree support needs.</li>
+							<li>Set targets for future actions</li>
+							<li>
+								Create networking opportunities for mentees to gain experience.
+							</li>
+						</ul>
+					</section>
+					<section>
+						<h3>Ending the Relationship</h3>
+						<ul>
+							<li>
+								Mentoring should end six months after Demo Day or when a mentee
+								secures employment.
+							</li>
+							<li>Summarise and celebrate achievements</li>
+							<li>Help your mentee to identify their next steps </li>
+							<li>
+								Both mentee and mentor should reflect on the effectiveness of
+								the relationship.
+							</li>
+						</ul>
+					</section>
+					<br></br>
+					<br></br>
+					<br></br>
+					<br></br>
+					<br></br>
+					<br></br>
+					<br></br>
 				</MentorsGuideStyles>
-			</div>
+			</main>
 		);
 };
 export default MentorsGuide;
--- client/src/pages/MentorsGuide.styles.js
+++ client/src/pages/MentorsGuide.styles.js
@@ -1,21 +1,21 @@
 import styled from "styled-components";
 
 export const MentorsGuideStyles = styled.div`
-	bode {
-		width: 100%;
-		height: 100%;
-		overflow: hidden;
-		display: flex;
-		flex-flow: row wrap;
-	}
 	h2 {
-		text-align: center;
-		margin-top: 4rem;
-		margin-bottom: -0.5rem;
+		margin: 4% 0% 0% 8%;
+	}
+	h3 {
+		text-align: left;
+		margin: 3% 0% 0% 8%;
 	}
 	p {
 		text-align: justify;
-		padding: 3rem 4rem 3rem 4rem;
+		margin: 1% 0% 0% 8%;
 		font-size: larger;
 	}
+	ul {
+		list-style-type: circle;
+		margin: 0% 0% 0% 8%;
+	}
+	
 `;
